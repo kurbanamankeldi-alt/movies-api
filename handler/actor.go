19f3df19d787d9d98d83fe074b72d083cea40897
Polina <polinafilippova314@gmail.com>
2026-07-26 16:19:42 +0300

    actors handler for get all

--- handler/actor.go
+++ handler/actor.go
@@ -1,1 +1,29 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/kurbanamankeldi-alt/movies-api/service"
+)
+
+type ActorHandler struct {
+	service *service.ActorService
+}
+
+func NewActorHandler(service *service.ActorService) *ActorHandler {
+	return &ActorHandler{service: service}
+}
+func (h *ActorHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	actors, err := h.service.GetAll()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(actors)
+}
--- main.go
+++ main.go
@@ -2,36 +2,42 @@ package main
 
 import (
 	"fmt"
-    "log"
-    "net/http"	
+	"log"
+	"net/http"
+
 	"github.com/kurbanamankeldi-alt/movies-api/db"
+	"github.com/kurbanamankeldi-alt/movies-api/handler"
 	"github.com/kurbanamankeldi-alt/movies-api/repository"
 	"github.com/kurbanamankeldi-alt/movies-api/service"
-	"github.com/kurbanamankeldi-alt/movies-api/handler"
 )
 
 func main() {
 	//initialize database
-    database, err := db.Init()
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer database.Close()
+	database, err := db.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
 
 	//seed with sample data
 	err = db.SeedTables(database)
 	if err != nil {
 		panic(err)
-	}	
+	}
 
 	port := "8081"
 	repo := repository.NewSQLiteMovieRepository(database)
-	service := service.NewMovieService(repo)
-	handler := handler.NewMovieHandler(service)	
+	movieService := service.NewMovieService(repo)
+	movieHandler := handler.NewMovieHandler(movieService)
+	//actors
+	actorRepo := repository.NewSQLiteActorRepository(database)
+	actorService := service.NewActorService(actorRepo)
+	actorHandler := handler.NewActorHandler(actorService)
 	mux := http.NewServeMux()
-	mux.HandleFunc("/api/movie/", handler.GetById)	
-	mux.HandleFunc("/api/movie", handler.CreateMovie)
+	mux.HandleFunc("/api/movie/", movieHandler.GetById)
+	mux.HandleFunc("/api/movie", movieHandler.CreateMovie)
+	mux.HandleFunc("/api/actors", actorHandler.GetAll)
 	fmt.Println("Server is running in the below link:")
 	fmt.Printf("http://localhost:%s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))	
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
