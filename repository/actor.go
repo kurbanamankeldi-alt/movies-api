203aa9a24dbe82426e273e31e99773ef54f2724f
Polina <polinafilippova314@gmail.com>
2026-07-26 15:57:07 +0300

    created some methods for actors

--- repository/actor.go
+++ repository/actor.go
@@ -1,1 +1,53 @@
 package repository
+
+import (
+	"database/sql"
+	"time"
+
+	"github.com/kurbanamankeldi-alt/movies-api/entity"
+)
+
+type SQLiteActorRepository struct {
+	db *sql.DB
+}
+
+func NewSQLiteActorRepository(db *sql.DB) *SQLiteActorRepository {
+	return &SQLiteActorRepository{db: db}
+}
+
+type ActorRepository interface {
+	Create(actor *entity.Actor) (int64, error)
+	GetAll() ([]entity.Actor, error)
+}
+
+func (a *SQLiteActorRepository) Create(actor *entity.Actor) (int64, error) {
+	sql := `INSERT INTO actors (name, birthdate) VALUES (?, ?);`
+
+	result, err := a.db.Exec(sql, actor.Name, actor.BirthDate.Format("2006-01-02"))
+	if err != nil {
+		return 0, err
+	}
+	return result.LastInsertId()
+}
+func (a *SQLiteActorRepository) GetAll() ([]entity.Actor, error) {
+	sql := `SELECT id, name, birthdate FROM actors`
+	rows, err := a.db.Query(sql)
+	if err != nil {
+		return []entity.Actor{}, err
+	}
+	defer rows.Close()
+	actors := []entity.Actor{}
+	for rows.Next() {
+		var id uint
+		var name, birthdate string
+		if err := rows.Scan(&id, &name, &birthdate); err != nil {
+			return []entity.Actor{}, err
+		}
+		birthTime, err := time.Parse("2006-01-02", birthdate)
+		if err != nil {
+			return []entity.Actor{}, err
+		}
+		actors = append(actors, entity.Actor{Id: id, Name: name, BirthDate: birthTime})
+	}
+	return actors, nil
+}
