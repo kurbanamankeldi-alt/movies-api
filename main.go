608554032e4db36f9dce1a28c2dca977bd61a076
Polina <polinafilippova314@gmail.com>
2026-07-27 17:56:24 +0300

    rebase

--- main.go
+++ main.go
@@ -2,38 +2,43 @@ package main
 
 import (
 	"fmt"
-    "log"
-    "net/http"	
+	"log"
+	"net/http"
+
 	"github.com/kurbanamankeldi-alt/movies-api/db"
+	"github.com/kurbanamankeldi-alt/movies-api/errors"
+	"github.com/kurbanamankeldi-alt/movies-api/handler"
 	"github.com/kurbanamankeldi-alt/movies-api/repository"
 	"github.com/kurbanamankeldi-alt/movies-api/service"
-	"github.com/kurbanamankeldi-alt/movies-api/handler"
-	"github.com/kurbanamankeldi-alt/movies-api/errors"
 )
 
 func main() {
 	//initialize database
-    database, err := db.Init()
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer database.Close()
+	database, err := db.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer database.Close()
 
 	//seed with sample data
 	err = db.SeedTables(database)
 	if err != nil {
 		panic(err)
-	}	
+	}
 
 	port := "8081"
 	repo := repository.NewSQLiteMovieRepository(database)
-	service := service.NewMovieService(repo)
-	handler := handler.NewMovieHandler(service)	
+	movieService := service.NewMovieService(repo)
+	movieHandler := handler.NewMovieHandler(movieService)
+	//actors
+	actorRepo := repository.NewSQLiteActorRepository(database)
+	actorService := service.NewActorService(actorRepo)
+	actorHandler := handler.NewActorHandler(actorService)
 	mux := http.NewServeMux()
 	mux.Handle("GET /api/movie", errors.HttpErrorHandler(handler.Get))
 	mux.Handle("GET /api/movie/{id}", errors.HttpErrorHandler(handler.GetById))
 	mux.Handle("POST /api/movie", errors.HttpErrorHandler(handler.Create))
 	fmt.Println("Server is running in the below link:")
 	fmt.Printf("http://localhost:%s\n", port)
-	log.Fatal(http.ListenAndServe(":"+port, mux))	
+	log.Fatal(http.ListenAndServe(":"+port, mux))
 }
--- service/actor.go
+++ service/actor.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"github.com/kurbanamankeldi-alt/movies-api/entity"
+	"github.com/kurbanamankeldi-alt/movies-api/repository"
+)
+
+type ActorService struct {
+	repo repository.ActorRepository
+}
+
+func NewActorService(repo repository.ActorRepository) *ActorService {
+	return &ActorService{
+		repo: repo,
+	}
+}
+func (s *ActorService) GetAll() ([]entity.Actor, error) {
+	actors, err := s.repo.GetAll()
+	if err != nil {
+		return []entity.Actor{}, err
+	}
+	return actors, nil
+}
