706e3d5781cab1e41d5a2f0bc6b9e0cbed185984
Polina <polinafilippova314@gmail.com>
2026-08-04 08:30:00 +0300

    added error handling for handlers

--- handler/actor.go
+++ handler/actor.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 
 	"github.com/kurbanamankeldi-alt/movies-api/entity"
+	"github.com/kurbanamankeldi-alt/movies-api/errors"
 	"github.com/kurbanamankeldi-alt/movies-api/service"
 )
 
@@ -16,93 +17,87 @@ type ActorHandler struct {
 func NewActorHandler(service *service.ActorService) *ActorHandler {
 	return &ActorHandler{service: service}
 }
-func (h *ActorHandler) GetAll(w http.ResponseWriter, r *http.Request) {
+func (h *ActorHandler) GetAll(w http.ResponseWriter, r *http.Request) *errors.HttpError {
 	name := r.URL.Query().Get("name")
 
 	if name == "" {
 		actors, err := h.service.GetAll()
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			return &errors.HttpError{Err: err, Message: err.Error(), Code: http.StatusInternalServerError}
 		}
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(actors)
-		return
+		return nil
 	}
 	actors, err := h.service.GetByName(name)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return &errors.HttpError{Err: err, Message: err.Error(), Code: http.StatusInternalServerError}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(actors)
+	return nil
 }
-func (h *ActorHandler) Create(w http.ResponseWriter, r *http.Request) {
+func (h *ActorHandler) Create(w http.ResponseWriter, r *http.Request) *errors.HttpError {
 	var actor entity.Actor
 	err := json.NewDecoder(r.Body).Decode(&actor)
 	if err != nil {
-		http.Error(w, "invalid json", http.StatusBadRequest)
-		return
+		return &errors.HttpError{Err: err, Message: "invalid json", Code: http.StatusBadRequest}
 	}
 	id, err := h.service.CreateActor(&actor)
 	actor.Id = uint(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return &errors.HttpError{Err: err, Message: err.Error(), Code: http.StatusInternalServerError}
 	}
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(actor)
+	return nil
 }
-func (h *ActorHandler) GetByID(w http.ResponseWriter, r *http.Request) {
+func (h *ActorHandler) GetByID(w http.ResponseWriter, r *http.Request) *errors.HttpError {
 	idStr := r.PathValue("id")
 	id, err := strconv.Atoi(idStr)
 	if err != nil {
-		http.Error(w, "invalid id", http.StatusBadRequest)
-		return
+		return &errors.HttpError{Err: err, Message: "invalid id", Code: http.StatusBadRequest}
 	}
 	actor, err := h.service.GetByID(id)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return &errors.HttpError{Err: err, Message: err.Error(), Code: http.StatusInternalServerError}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(actor)
+	return nil
 }
-func (h *ActorHandler) Update(w http.ResponseWriter, r *http.Request) {
+func (h *ActorHandler) Update(w http.ResponseWriter, r *http.Request) *errors.HttpError {
 	idActor := r.PathValue("id")
 	id, err := strconv.Atoi(idActor)
 	if err != nil {
-		http.Error(w, "invalid id", http.StatusBadRequest)
-		return
+		return &errors.HttpError{Err: err, Message: "invalid id", Code: http.StatusBadRequest}
 	}
 	var actorUpdate entity.ActorPatchRequest
 	err1 := json.NewDecoder(r.Body).Decode(&actorUpdate)
 	if err1 != nil {
-		http.Error(w, "invalid json", http.StatusBadRequest)
-		return
+		return &errors.HttpError{Err: err, Message: "invalid json", Code: http.StatusBadRequest}
 	}
 	actor, err := h.service.Update(id, actorUpdate)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return &errors.HttpError{Err: err, Message: err.Error(), Code: http.StatusInternalServerError}
 	}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(actor)
+	return nil
 }
-func (h *ActorHandler) Delete(w http.ResponseWriter, r *http.Request) {
+func (h *ActorHandler) Delete(w http.ResponseWriter, r *http.Request) *errors.HttpError {
 	idActor := r.PathValue("id")
 	id, err := strconv.Atoi(idActor)
 	if err != nil {
-		http.Error(w, "invalid id", http.StatusBadRequest)
-		return
+		return &errors.HttpError{Err: err, Message: "invalid id", Code: http.StatusBadRequest}
 	}
 	gotForce := r.URL.Query().Get("force")
 	force := gotForce == "true"
 	err = h.service.Delete(id, force)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
+		return &errors.HttpError{Err: err, Message: err.Error(), Code: http.StatusInternalServerError}
 	}
 	w.WriteHeader(http.StatusNoContent)
+	return nil
 }
--- server/routes.go
+++ server/routes.go
@@ -24,9 +24,9 @@ func RegisterRoutes(mux *http.ServeMux, database *sql.DB) {
 	mux.Handle("GET /api/movie/{id}", errors.HttpErrorHandler(movieHandler.GetById))
 	mux.Handle("POST /api/movie", errors.HttpErrorHandler(movieHandler.Create))
 
-	mux.HandleFunc("GET /api/actors", actorHandler.GetAll)
-	mux.HandleFunc("POST /api/actors", actorHandler.Create)
-	mux.HandleFunc("GET /api/actors/{id}", actorHandler.GetByID)
-	mux.HandleFunc("PATCH /api/actors/{id}", actorHandler.Update)
-	mux.HandleFunc("DELETE /api/actors/{id}", actorHandler.Delete)
+	mux.Handle("GET /api/actors", errors.HttpErrorHandler(actorHandler.GetAll))
+	mux.Handle("POST /api/actors", errors.HttpErrorHandler(actorHandler.Create))
+	mux.Handle("GET /api/actors/{id}", errors.HttpErrorHandler(actorHandler.GetByID))
+	mux.Handle("PATCH /api/actors/{id}", errors.HttpErrorHandler(actorHandler.Update))
+	mux.Handle("DELETE /api/actors/{id}", errors.HttpErrorHandler(actorHandler.Delete))
 }
